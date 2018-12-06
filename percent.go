46fba4e883e142967666218a13829950801c25da
Dariush Abbasi <dariushem@yahoo.com>
2018-12-06 12:15:19 +0330

    add test for PercentFloat func

--- percent.go
+++ percent.go
@@ -3,34 +3,34 @@ package percent
 // Percent calculate what is [percent]% of [number] using int values.
 // For Example 25% of 200 is 50
 // It returns result as float64
-func Percent(pcent int, all int) float64{
-  percent := ((float64(all) * float64(pcent)) / float64(100))
-  return percent
+func Percent(pcent int, all int) float64 {
+	percent := ((float64(all) * float64(pcent)) / float64(100))
+	return percent
 }
 
-// Percent calculate what is [percent]% of [number] using float values.
+// PercentFloat calculate what is [percent]% of [number] using float values.
 // For Example 0.3% of 200 is 0.6
 // It returns result as float64
-func PercentFloat(pcent float64, all int) float64{
-  percent := ((float64(all) * float64(pcent)) / float64(100))
-  return percent
+func PercentFloat(pcent float64, all int) float64 {
+	percent := ((float64(all) * float64(pcent)) / float64(100))
+	return percent
 }
 
 // PercentOf calculate [number1] is what percent of [number2]
 // For example 300 is 12.5% of 2400
 // It returns result as float64
 func PercentOf(current int, all int) float64 {
-  percent := ( float64(current) * float64(100) ) / float64(all)
-  return percent
+	percent := (float64(current) * float64(100)) / float64(all)
+	return percent
 }
 
 // Change calculate what is the percentage increase/decrease from [number1] to [number2]
 // For example 60 is 200% increase from 20
 // It returns result as float64
-func Change(before int, after int) float64{
-  diff := float64(after) - float64(before)
-  realDiff := diff / float64(before)
-  percentDiff := 100 * realDiff
+func Change(before int, after int) float64 {
+	diff := float64(after) - float64(before)
+	realDiff := diff / float64(before)
+	percentDiff := 100 * realDiff
 
-  return percentDiff
+	return percentDiff
 }
--- percent_test.go
+++ percent_test.go
@@ -1,32 +1,41 @@
 package percent
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestPercent(t *testing.T) {
-  pcent, all := 25, 2000
-  part := Percent(pcent, all)
+	pcent, all := 25, 2000
+	part := Percent(pcent, all)
 
-  if part != 500.0 {
-    t.Fatalf("%d is Wrong number for %d percent!", int(part), pcent)
-  }
+	if part != 500.0 {
+		t.Fatalf("%d is Wrong number for %d percent!", int(part), pcent)
+	}
+}
+
+func TestPercentFloat(t *testing.T) {
+	pcent, all := 25.0, 2000
+	part := PercentFloat(pcent, all)
+
+	if part != 500.0 {
+		t.Fatalf("%d is Wrong number for %f percent!", int(part), pcent)
+	}
 }
 
 func TestPercentOf(t *testing.T) {
-  part, all := 20, 50
-  pcent := PercentOf(part, all)
+	part, all := 20, 50
+	pcent := PercentOf(part, all)
 
-  if pcent != 40 {
-    t.Fatalf("Wrong percent!")
-  }
+	if pcent != 40 {
+		t.Fatalf("Wrong percent!")
+	}
 }
 
 func TestChange(t *testing.T) {
-  before, after := 25, 200
-  pcent := Change(before, after)
+	before, after := 25, 200
+	pcent := Change(before, after)
 
-  if int(pcent) != 700 {
-    t.Fatalf("%f is Wrong percent!", pcent)
-  }
+	if int(pcent) != 700 {
+		t.Fatalf("%f is Wrong percent!", pcent)
+	}
 }
