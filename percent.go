62cc0482f03ea4b795db3858159f465f288e2a7f
Faizan Khalid <IamFaizanKhalid@gmail.com>
2024-03-13 14:05:30 +0500

    Divide By Zero Protection

--- percent.go
+++ percent.go
@@ -15,23 +15,35 @@ func PercentFloat(percent float64, all float64) float64 {
 // PercentOf - calculate what percent [number1] is of [number2].
 // ex. 300 is 12.5% of 2400
 func PercentOf(part int, total int) float64 {
+	if total == 0 {
+		return 0
+	}
 	return (float64(part) * float64(100)) / float64(total)
 }
 
 // PercentOfFloat - calculate what percent [number1] is of [number2].
 // ex. 300 is 12.5% of 2400
 func PercentOfFloat(part float64, total float64) float64 {
+	if total == 0 {
+		return 0
+	}
 	return (float64(part) * float64(100)) / float64(total)
 }
 
 // Change - calculate the percent increase/decrease from two numbers.
 // ex. 60 is a 200.0% increase from 20
 func Change(before int, after int) float64 {
+	if before == 0 {
+		return float64(after)
+	}
 	return 100 * ((float64(after) - float64(before)) / float64(before))
 }
 
 // ChangeFloat - calculate the percent increase/decrease from two numbers.
 // ex. 60.0 is a 200.0% increase from 20.0
 func ChangeFloat(before float64, after float64) float64 {
+	if before == 0 {
+		return after
+	}
 	return 100 * ((after - before) / float64(before))
 }
